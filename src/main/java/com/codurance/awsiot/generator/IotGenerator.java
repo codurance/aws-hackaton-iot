149d9c6d73a4775061a524be82d79426b0e5df24
Maciej Kowalski <maciej.kowalski@codurance.com>
2020-11-18 12:21:11 +0000

    Date generation and temperature generation

--- src/main/java/com/codurance/awsiot/generator/IotGenerator.java
+++ src/main/java/com/codurance/awsiot/generator/IotGenerator.java
@@ -8,28 +8,72 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 @Component
 public class IotGenerator {
 
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+
     @Scheduled(fixedDelay = 5000)
     public void generate() {
         IoTData data = new IoTData();
-        data.setDate();
-        data.setTemperature();
-        data.setTxt();
+        data.setDate(generateDate());
+        data.setTemperature(generateTemperature());
+        getClass();
+//        data.setTxt();
     }
+
+    private String generateTemperature(){
+        StringBuilder temperatureBuilder = new StringBuilder();
+
+        Random celciusOrFarenheitRandom = new Random();
+        int celciusOrFarenheitResult = celciusOrFarenheitRandom.nextInt(3);
+
+        if(celciusOrFarenheitResult == 1){
+            Random celciusRandom = new Random();
+            int addition = celciusRandom.nextInt(81);
+
+            temperatureBuilder.append(-30 + addition);
+            temperatureBuilder.append("C");
+        }else{
+            Random farengheitRandom = new Random();
+            int addition = farengheitRandom.nextInt(101);
+
+            temperatureBuilder.append(-22 + addition);
+            temperatureBuilder.append("F");
+        }
+
+        return temperatureBuilder.toString();
+    }
+
     private String generateDate() {
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
         LocalDateTime localNow = LocalDateTime.now();
-        // setting UTC as the timezone
-        ZonedDateTime zonedUTC = localNow.atZone(ZoneId.of("UTC"));
-        // converting to IST
-        ZonedDateTime zonedIST = zonedUTC.withZoneSameInstant(ZoneId.of("Asia/Kolkata"));
-        ZonedDateTime zonedPST = zonedUTC.withZoneSameInstant(ZoneId.of("America/Los_Angeles"));
 
-        String localDateTime = LocalDateTime.now().format(formatter);
-        return
+        ZonedDateTime zonedUTC = localNow.atZone(ZoneId.of("UTC"));
+
+        Random random = new Random();
+        int option = random.nextInt(100) % 3;
+        String difference = "+0";
+
+        switch (option){
+            case 0: break;
+
+            case 1: {
+                zonedUTC = zonedUTC.withZoneSameInstant(ZoneId.of("Asia/Tokyo"));
+                difference = "+9";
+            };break;
+            case 2: {
+                zonedUTC = zonedUTC.withZoneSameInstant(ZoneId.of("America/Los_Angeles"));
+                difference = "-8";
+            }break;
+
+            default: throw new UnsupportedOperationException();
+        }
+
+        String formattedDateTime = zonedUTC.format(formatter).concat(difference);
+
+        return formattedDateTime;
     }
 }
