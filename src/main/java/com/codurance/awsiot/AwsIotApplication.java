1a588c0168e2517616c6052cb22a4f1aacaa900b
Maciej Kowalski <maciej.kowalski@codurance.com>
2020-11-18 11:24:14 +0000

    Initial structure

---
+++ src/main/java/com/codurance/awsiot/AwsIotApplication.java
@@ -0,0 +1,13 @@
+package com.codurance.awsiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsIotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codurance/awsiot/AwsIotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codurance.awsiot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsIotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
